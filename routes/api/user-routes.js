be607d740880eaa83fa4e231bbab2de9454a9f1a
KDash201 <KVonMadison@Yahoo.com>
2022-03-06 16:21:27 -0500

    deleted user api call successfully

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,13 +4,13 @@ const {
   getUserById,
   createUser,
   updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
 // /api/users
 router.route("/").get(getAllUser).post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser);
-// .delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
